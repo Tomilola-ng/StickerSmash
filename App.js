b35d5112f7cec5550eb0b6aacca6fda855a87065
Tomilola-ng <tee.o2809@gmail.com>
2024-03-24 08:25:18 +0100

    UPDATE_STRINGs

--- App.js
+++ App.js
@@ -41,7 +41,7 @@ export default function App() {
       setSelectedImage(result.assets[0].uri);
       setShowAppOptions(true);
     } else {
-      alert("You did not select any image.");
+      alert("You need to select an image.");
     }
   };
 
